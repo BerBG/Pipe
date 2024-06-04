a4719df176211ef2f2e5bdbdbcdcf096ae600885
Bernardo Portes <47006773+BerBG@users.noreply.github.com>
2024-06-04 20:35:31 -0300

    Update RetanguloTeste.java

--- src/test/java/Retangulo/RetanguloTeste.java
+++ src/test/java/Retangulo/RetanguloTeste.java
@@ -8,7 +8,7 @@ public class RetanguloTeste {
     @Test
     public void testArea() {
         Retangulo retangulo = new Retangulo(5, 10);
-        assertEquals(40, retangulo.area());
+        assertEquals(50, retangulo.area());
     }
 
     @Test
